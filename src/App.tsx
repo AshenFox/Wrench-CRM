28b904b859601746d0bffa3828bc2add6321d76f
Max_K <kavokinm@gmail.com>
2022-10-26 21:14:20 +0300

    Putting together menu layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Calender from './components/Icons/Calender';
+import CogWheel from './components/Icons/CogWheel';
+import Exit from './components/Icons/Exit';
+import House from './components/Icons/House';
+import MagnifyingGlass from './components/Icons/MagnifyingGlass';
+import MapPoint from './components/Icons/MapPoint';
 import Person from './components/Icons/Person';
 import PersonFrame from './components/Icons/PersonFrame';
+import Tables from './components/Icons/Tables';
+import TV from './components/Icons/TV';
 import Wrench from './components/Icons/Wrench';
 
 const App = () => {
@@ -20,8 +28,69 @@ const App = () => {
           </div>
         </div>
       </header>
-      <main className='main'></main>
-      <menu className='menu'></menu>
+
+      <div className='container'>
+        <menu className='menu'>
+          <h3 className='menu__header'>Меню</h3>
+          {/* ======== */}
+          <div className='menu__item'>
+            <div className='menu__item-icon'>
+              <House />
+            </div>
+            <span className='menu__item-title'>Главная</span>
+          </div>
+          {/* ======== */}
+          <div className='menu__item'>
+            <div className='menu__item-icon'>
+              <MagnifyingGlass />
+            </div>
+            <span className='menu__item-title'>Поиск адресов</span>
+          </div>
+          {/* ======== */}
+          <div className='menu__item'>
+            <div className='menu__item-icon'>
+              <Tables />
+            </div>
+            <span className='menu__item-title'>Таблицы</span>
+          </div>
+          {/* ======== */}
+          <div className='menu__item'>
+            <div className='menu__item-icon'>
+              <Calender />
+            </div>
+            <span className='menu__item-title'>Календарь</span>
+          </div>
+          {/* ======== */}
+          <div className='menu__item'>
+            <div className='menu__item-icon'>
+              <MapPoint />
+            </div>
+            <span className='menu__item-title'>Карты</span>
+          </div>
+          {/* ======== */}
+          <div className='menu__item'>
+            <div className='menu__item-icon'>
+              <TV />
+            </div>
+            <span className='menu__item-title'>Виджеты</span>
+          </div>
+          {/* ======== */}
+          <div className='menu__item'>
+            <div className='menu__item-icon'>
+              <CogWheel />
+            </div>
+            <span className='menu__item-title'>Настройки</span>
+          </div>
+          {/* ======== */}
+          <div className='menu__item'>
+            <div className='menu__item-icon'>
+              <Exit />
+            </div>
+            <span className='menu__item-title'>Выход</span>
+          </div>
+        </menu>
+        <main className='main'></main>
+      </div>
     </>
   );
 };
